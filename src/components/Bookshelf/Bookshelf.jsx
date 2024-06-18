5f30d0284fedd53ffd7ae232347c6008c9e95a94
Jordan Christensen <33182552+JordanChristensen@users.noreply.github.com>
2024-06-18 14:31:52 -0700

    improve UI and semantic HTML usage

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+// import Book from "./Book";
+// import BookshelfDataBookAuthor from "./BookshelfDataBookAuthor";
+// import BookshelfDataBookTitle from "./BookshelfDataBookTitle";
 
 const Bookshelf = () => {
   const [books, setBooks] = useState([
@@ -6,37 +9,48 @@ const Bookshelf = () => {
     { title: "The Lion, the Witch and the Wardrobe", author: "C.S. Lewis" },
   ]);
 
-  const handleChange = (e) => {
-    setBooks(e.target.value);
+  const handleFormInputChange = (e) => {
+    return setBooks({ ...books, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log("Default form submission has been prevented.");
   };
 
   return (
-    <div className="bookshelfDiv">
-      <div className="formDiv">
-        <h3>Add a Book</h3>
-        {/* Form will go here */}
-        <form action="">
-          <label htmlFor="bookTitle">Title: </label>
-          <input
-            id="bookTitle"
-            name="bookTitle"
-            type="text"
-            value={books.Title}
-            onChange={handleChange}
-          />
-          <label htmlFor="bookAuthor">Author: </label>
-          <input
-            id="bookAuthor"
-            name="bookAuthor"
-            type="text"
-            value={books.Author}
-            onChange={handleChange}
-          />
-          <button type="submit">Add book</button>
-        </form>
-      </div>
-      <div className="bookCardsDiv">{/* Book cards will display here */}</div>
-    </div>
+    <section>
+      <h1>My bookshelf</h1>
+      <section className="bookshelfDiv">
+        <section className="formDiv">
+          <h2>Add a Book</h2>
+          <form>
+            <label htmlFor="bookTitle">Title: </label>
+            <input
+              id="bookTitle"
+              name="bookTitle"
+              type="text"
+              value={books.title}
+              onChange={handleFormInputChange}
+            />
+            <label htmlFor="bookAuthor">Author: </label>
+            <input
+              id="bookAuthor"
+              name="bookAuthor"
+              type="text"
+              value={books.author}
+              onChange={handleFormInputChange}
+            />
+            <button type="submit">Add book</button>
+          </form>
+        </section>
+        <section className="bookCardsDiv" onSubmit={handleSubmit}>
+          {/* Book cards will display here */}
+          
+          {/* <Book title={books.title} /> */}
+        </section>
+      </section>
+    </section>
   );
 };
 
